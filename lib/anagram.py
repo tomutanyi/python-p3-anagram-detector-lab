702ddf91e2ec684782c999ffdfae0792865d9593
tomutanyi <tomutanyi@gmail.com>
2023-09-19 23:57:26 +0300

    Completed assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+class Anagram:
+    def __init__(self,word):
+        self.word = word
+    
+    def match(self,input_list):
+        anagram=[]
+        for word in input_list:
+            if sorted(word) == sorted(self.word):
+                anagram.append(word)
+        return anagram
+        
+print(Anagram("enlist").match(["listen", "poison", "hello"]))
